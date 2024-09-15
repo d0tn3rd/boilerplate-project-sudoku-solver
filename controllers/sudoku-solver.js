345b57047631d0b15327ab80870b6d0f29e9ef61
d0tn3rd <d0tn3rd@mailbox.org>
2024-09-15 17:58:09 +0530

    implement validation function

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,36 @@
+const REGION_SIZE = 3;
+
+const REGION_START_ROW = 0;
+
+const REGION_START_COL = 0;
+
+const REGION_END_ROW = 2;
+
+const REGION_END_COL = 2;
+
+const REGION_START_ROW_OFFSET = 0;
+
+const REGION_START_COL_OFFSET = 0;
 class SudokuSolver {
-
   validate(puzzleString) {
+    const puzzleRegex = /[1-9\.]{81}/;
+    const matches = puzzleString.match(puzzleRegex);
+
+    return Boolean(matches);
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
-
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
   solve(puzzleString) {
-    
+    const isValidPuzzle = this.validate(puzzleString);
+    if (!isValidPuzzle) {
+      return "error";
+    }
   }
 }
 
 module.exports = SudokuSolver;
-
