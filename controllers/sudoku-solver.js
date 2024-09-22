fae406474f794e672bd725bbafca1e38780da6c5
d0tn3rd <d0tn3rd@mailbox.org>
2024-09-22 16:01:01 +0530

    complete implementation of solver

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,7 +1,15 @@
 const ROW_HEADING = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+
+const globalOptsSet = new Set(
+  [1, 2, 3, 4, 5, 6, 7, 8, 9].map((s) => String(s)),
+);
+
+const debug = process.env.DEBUG || true;
+console.debug = () => {};
+
 class SudokuSolver {
   validate(puzzleString) {
-    const puzzleRegex = /[1-9\.]{81}/;
+    const puzzleRegex = /[1-9\.]+/;
     const matches = puzzleString.match(puzzleRegex);
 
     if (puzzleString.length !== 81)
@@ -9,56 +17,56 @@ class SudokuSolver {
 
     if (!matches) return { error: "Invalid characters in puzzle" };
 
-    return { error: false, success: true };
+    return { error: false }; // no error means success
   }
 
   _validateCoordinates(row, col) {
     // validate row / col values
     if (ROW_HEADING.indexOf(row) === -1) return { error: "Invalid coordinate" };
 
-    if ([1, 2, 3, 4, 5, 6, 7, 8, 9].indexOf(column) === -1)
+    const parsedCol = parseInt(col);
+    if ([1, 2, 3, 4, 5, 6, 7, 8, 9].indexOf(parsedCol) === -1)
       return { error: "Invalid coordinate" };
     if (ROW_HEADING.indexOf(row) === -1) {
       return { error: "Invalid coordinate" };
     }
-    const parsedCol = parseInt(col);
-    if (!parsedCol) {
+    if (!parsedCol || parsedCol < 1 || parsedCol > 9) {
       return { error: "Invalid coordinate" };
     }
-    if (parsedCol < 1 || parsedCol > 9) {
-      return { error: "Invalid coordinate" };
-    }
+
+    return { error: false };
   }
 
   _calculateRowIndices(row) {
     // this function returns the current row's indices on the puzzlestring
     // convert to indices for calculation
     const rowIndex = ROW_HEADING.indexOf(row);
-    // we throw away the col
 
+    const start = Math.floor(rowIndex / 9); // throw away the remainder
     const rowIndices = [];
     for (let i = 0; i < 9; i++) {
-      rowIndices.push(9 * rowIndex + i);
+      rowIndices.push(start + i);
     }
 
     return rowIndices;
   }
 
-  _calculateColumnIndices(col) {
+  _calculateColumnIndices(index) {
     // return current columns indinces on the puzzlestring
-    const colIndex = col - 1;
+    const colIndex = index % 9;
     const colIndices = [];
     for (let i = 0; i < 9; i++) {
       colIndices.push(9 * i + colIndex);
     }
+    return colIndices;
   }
 
-  _calculateRegionIndices(row, col) {
+  _calculateRegionIndices(index) {
     // calculate region's indices no the puzzleString
-    const rowIndex = ROW_HEADING.indexOf(row);
-    const colIndex = parseInt(col) - 1;
+    const rowIndex = Math.floor(index / 9);
+    const colIndex = index % 9;
     const rowStart = Math.floor(rowIndex / 3);
-    const regionMultiplier = Math.Floor(colIndex / 3);
+    const regionMultiplier = Math.floor(colIndex / 3);
     const regionOffset = regionMultiplier * 3;
     const regionIndices = [];
     for (let i = 0; i < 3; i++) {
@@ -77,42 +85,230 @@ class SudokuSolver {
     return rowIndex * 9 + col - 1;
   }
 
+  _calculateColumns() {
+    // will return an array of 9 element columns as arrays
+    // ideally should be called only once
+    const puzzleStrArr = [...this.puzzleStrArr];
+    const cols = [];
+    for (let colIndex = 0; colIndex < 9; colIndex++) {
+      const col = [];
+      for (let rowIndex = 0; rowIndex < 81; rowIndex = rowIndex + 9) {
+        col.push(puzzleStrArr[rowIndex + colIndex]);
+      }
+      cols.push(col);
+    }
+
+    return cols;
+  }
+
+  _calculateRows() {
+    const puzzleStrArr = [...this.puzzleStrArr];
+    const rows = [];
+    for (let i = 0; i < 81; i = i + 9) {
+      rows.push(puzzleStrArr.slice(i, i + 9));
+    }
+    return rows;
+  }
+
+  _calculateRegions() {
+    // choosing the bottom left cell as the anchor
+    const puzzleStrArr = [...this.puzzleStrArr];
+    const regions = [];
+
+    // increasing index
+    for (const regionStartIndex of [0, 3, 6, 27, 30, 33, 54, 57, 60]) {
+      const region = [];
+      for (let rowOffset = 0; rowOffset < 3; rowOffset++) {
+        for (let colOffset = 0; colOffset < 3; colOffset++) {
+          region.push(
+            puzzleStrArr[regionStartIndex + rowOffset * 9 + colOffset],
+          );
+        }
+      }
+      regions.push(region);
+    }
+
+    return regions;
+  }
+
+  _findRegionIndex(puzzleStrIndex) {
+    const rowIndex = Math.floor(puzzleStrIndex / 9);
+    const colIndex = puzzleStrIndex % 9;
+
+    const regionOptions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+    ];
+    const regionIndex =
+      regionOptions[Math.floor(puzzleStrIndex / 27)][Math.floor(colIndex / 3)];
+
+    return regionIndex;
+  }
+
   checkRowPlacement(puzzleString, row, column, value) {
     const { error } = this._validateCoordinates(row, column);
     if (error) return { error };
+    // validated
+    const rowIndex = ROW_HEADING.indexOf(row);
+    const rowsToBeChecked = this._calculateRows(puzzleString.split(""));
+    const targetRow = rowsToBeChecked[rowIndex];
 
-    // find row indices
-    const rowIndices = this._calculateRowIndices(row, column);
-    const rowVals = rowIndices.map((index) => puzzleString[index]);
-    if (rowVals.indexOf(value) !== -1) {
-      return false;
-    }
-    return true;
+    return {
+      error: targetRow.indexOf(value) !== -1,
+    };
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkColumnPlacement(puzzleString, row, column, value) {
     const { error } = this._validateCoordinates(row, column);
     if (error) return { error };
 
-    // find col indices
-    const colIndices = this._calculateColumnIndices(row, column);
-    const colVals = colIndices.map((index) => puzzleString[index]);
-    return colVals.indexOf(value !== -1);
+    const colIndex = column - 1;
+    const colsToBeChecked = this._calculateColumns(puzzleString.split(""));
+    const targetCol = colsToBeChecked[colIndex];
+
+    return {
+      error: targetCol.indexOf(value) !== -1,
+    };
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-    // assume val is string
     const { error } = this._validateCoordinates(row, column);
+
     if (error) return { error };
 
-    const regionIndices = this._calculateRegionIndices(row, column);
-    const regionVals = regionIndices.map((index) => puzzleString[index]);
-    return regionVals.indexOf(value) !== -1;
+    const puzzleStringIndex = this._calculatePuzzleStringIndex(row, column);
+
+    const regionIndex = this._findRegion(puzzleStringIndex);
+    const allRegions = this._calculateRegions(puzzleString.split(""));
+
+    const targetRegion = allRegions[regionIndex];
+
+    return { error: targetRegion.indexOf(value) !== -1 };
+  }
+
+  _setDifference(setA, setB) {
+    return new Set([...setA].filter((el) => !setB.has(el)));
+  }
+
+  _formatPuzzle(puzzleString) {
+    let startIndex = 0;
+    while (startIndex !== 81) {
+      console.log(
+        puzzleString
+          .slice(startIndex, startIndex + 9)
+          .split("")
+          .join(" "),
+      );
+      startIndex = startIndex + 9;
+    }
+  }
+
+  findMoves() {
+    // find a move
+    // assess row by row
+    for (let rowIndex = 0; rowIndex < 9; rowIndex++) {
+      const currentRow = [...this.rows[rowIndex]];
+
+      const rowNums = currentRow.filter((x) => x !== ".");
+
+      // indices of dots in row
+      const dotColIndices = currentRow.reduce((aggr, curr, index) => {
+        if (curr === ".") {
+          aggr.push(index);
+        }
+        return aggr;
+      }, []);
+
+      for (const colIndex of dotColIndices) {
+        const currCol = [...this.cols[colIndex]];
+        const colNums = currCol.filter((x) => x !== ".");
+
+        const puzzleStrIndex = 9 * rowIndex + colIndex;
+        const currRegionIndex = this._findRegionIndex(puzzleStrIndex);
+
+        const currRegion = this.regions[currRegionIndex];
+
+        const regionNums = currRegion.filter((x) => x !== ".");
+
+        // now remove them all from current dot's guessmap
+        const aggrSet = new Set(colNums + regionNums + rowNums);
+
+        const newOptions = this._setDifference(
+          this.optionMap[puzzleStrIndex],
+          aggrSet,
+        );
+
+        if (newOptions.size === 1) {
+          // found the move, no need to search further
+          return { puzzleStrIndex, val: Array.from(newOptions)[0] };
+        }
+      }
+    }
+
+    // nothing found
+    return null;
+  }
+
+  fillSpot(move) {
+    // hello world
+
+    const { puzzleStrIndex, val } = move;
+
+    // remove the key from the map
+
+    console.log("deleting key", puzzleStrIndex);
+    delete this.optionMap[puzzleStrIndex];
+    this.puzzleStrArr[puzzleStrIndex] = val;
+    this.rows = this._calculateRows();
+    this.cols = this._calculateColumns();
+    this.regions = this._calculateRegions();
+    console.log("new option map", this.optionMap);
   }
 
   solve(puzzleString) {
+    // validation
     const { error } = this.validate(puzzleString);
     if (error) return { error };
+
+    // setup
+
+    console.log("setting up the class");
+    this.puzzleStrArr = puzzleString.split("");
+    const dotIndices = [...this.puzzleStrArr].reduce((aggr, curr, index) => {
+      if (curr === ".") aggr.push(index);
+      return aggr;
+    }, []);
+
+    const optionMap = dotIndices.reduce((aggr, curr) => {
+      aggr[curr] = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9].map((el) => String(el)));
+      return aggr;
+    }, {});
+
+    const puzzleStrArr = [...this.puzzleStrArr];
+    this.optionMap = optionMap;
+    this.rows = this._calculateRows(puzzleStrArr);
+    this.cols = this._calculateColumns(puzzleStrArr);
+    this.regions = this._calculateRegions(puzzleStrArr);
+
+    console.log("Setup complete");
+    console.log("stored option map: ", this.optionMap);
+
+    // find the first move
+    let move = this.findMoves();
+    while (move) {
+      console.log("applying move: ", move);
+      this.fillSpot(move);
+      move = this.findMoves();
+    }
+
+    if (Object.keys(this.optionMap).length === 0) {
+      // solved
+      console.log("solution");
+      console.log(this.puzzleStrArr.join(""));
+    } else {
+      // cannot be solved
+    }
   }
 }
 
