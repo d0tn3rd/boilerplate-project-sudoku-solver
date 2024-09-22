7bec1dbbef4b2b9f8bbb664c72731e4fc9aa0197
d0tn3rd <d0tn3rd@mailbox.org>
2024-09-22 20:26:48 +0530

    implement /api/solve endpoint

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,29 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {});
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+  app.route("/api/solve").post((req, res) => {
+    if (!req.body.puzzle) {
+      return res.status(400).send({
+        error: "Required field missing",
+      });
+    }
 
-    });
+    const puzzleString = req.body.puzzle;
+
+    const result = solver.validate(puzzleString);
+
+    if (result.error) return res.status(400).send({ error: result.error });
+
+    const solved = solver.solve(puzzleString);
+
+    if (solved.error) return res.status(400).send({ error: solved.error });
+
+    return res.status(200).send({ solution: solved.solution });
+  });
 };
