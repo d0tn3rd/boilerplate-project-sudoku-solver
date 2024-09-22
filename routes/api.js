6fde693a9c8c84e6ae4cb022fc245a52c5e62585
d0tn3rd <d0tn3rd@mailbox.org>
2024-09-22 20:37:17 +0530

    implement /api/check/

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,36 @@ const SudokuSolver = require("../controllers/sudoku-solver.js");
 module.exports = function (app) {
   let solver = new SudokuSolver();
 
-  app.route("/api/check").post((req, res) => {});
+  app.route("/api/check").post((req, res) => {
+    const reqBody = req.body;
+
+    if (!reqBody.puzzle || !reqBody.coordinate || !reqBody.value)
+      return res.status(400).send({
+        error: "Required field(s) missing",
+      });
+
+    if (reqBody.coordinate.length !== 2) return { error: "Invalid coordinate" };
+    // check the coordinate
+
+    const { error } = solver._validateCoordinates(
+      reqBody.coordinate[0],
+      reqBody.coordinate[1],
+    );
+
+    if (error) return { error: "Invalid coordinate" };
+
+    if (!String(reqBody.value).match(/[1-9]{1}/g))
+      return { error: "Invalid value" };
+
+    const result = solver.check(
+      reqBody.puzzle,
+      reqBody.coordinate,
+      reqBody.value,
+    );
+    if (result.error) return res.status(400).send({ error: result.error });
+
+    return res.status(200).send({ result: result.result });
+  });
 
   app.route("/api/solve").post((req, res) => {
     if (!req.body.puzzle) {
